7e53ccafb0c8ea2fea29728965c3c5754c942e67
ellis.bywater <egaraudy@gmail.com>
2024-11-11 16:16:36 -0600

    build user feed; add pagination and filters;

---
+++ internal/store/pagination.go
@@ -0,0 +1,79 @@
+package store
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type PaginatedFeedQuery struct {
+	Limit  int      `json:"limit" validate:"gte=1,lte=20"`
+	Offset int      `json:"offset" validate:"gte=0"`
+	Sort   string   `json:"sort" validate:"oneof=asc desc"`
+	Tags   []string `json:"tags" validate:"max=5"`
+	Search string   `json:"search" validate:"max=100"`
+	Since  string   `json:"since"`
+	Until  string   `json:"until"`
+}
+
+func (q PaginatedFeedQuery) Parse(r *http.Request) (PaginatedFeedQuery, error) {
+	qs := r.URL.Query()
+
+	limit := qs.Get("limit")
+	if limit != "" {
+		l, err := strconv.Atoi(limit)
+		if err != nil {
+			return q, nil
+		}
+		if l > 20 {
+			l = 20
+		}
+		q.Limit = l
+	}
+	offset := qs.Get("offset")
+	if offset != "" {
+		o, err := strconv.Atoi(offset)
+		if err != nil {
+			return q, nil
+		}
+		if o > 10 {
+			o = 10
+		}
+		q.Offset = o
+	}
+
+	sort := qs.Get("sort")
+	if sort != "" {
+		q.Sort = sort
+	}
+
+	tags := qs.Get("tags")
+	if tags != "" {
+		q.Tags = strings.Split(tags, ",")
+	}
+
+	search := qs.Get("search")
+	if search != "" {
+		q.Search = search
+	}
+
+	since := qs.Get("since")
+	if since != "" {
+		q.Since = parseTime(since)
+	}
+	until := qs.Get("until")
+	if since != "" {
+		q.Until = parseTime(until)
+	}
+
+	return q, nil
+}
+
+func parseTime(s string) string {
+	t, err := time.Parse(time.DateTime, s)
+	if err != nil {
+		return ""
+	}
+	return t.Format(time.DateTime)
+}
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -18,6 +18,12 @@ type Post struct {
 	UpdatedAt string    `json:"updated_at"`
 	Version   int64     `json:"version"`
 	Comments  []Comment `json:"comments"`
+	User      User      `json:"user"`
+}
+
+type PostWithMetadata struct {
+	Post
+	CommentsCount int `json:"comments_count"`
 }
 
 type PostStore struct {
@@ -116,4 +122,52 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 		}
 	}
 	return nil
+}
+
+func (s *PostStore) GetUserFeed(ctx context.Context, userId int64, fq PaginatedFeedQuery) ([]PostWithMetadata, error) {
+	query := `
+		Select p.id, p.user_id, p.title, p.content, p.created_at, p.version, p.tags, u.username,
+		 COUNT(c.id) AS comments_count
+		FROM posts p
+		LEFT JOIN comments c ON c.post_id = p.id
+		LEFT JOIN users u ON p.user_id = u.id
+		JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
+		WHERE
+			f.user_id = $1 AND
+			(p.title ILIKE '%' || $4 || '%' OR p.content ILIKE '%' || $4 || '%') AND
+			(p.tags @> $5 OR $5 = '{}')
+		GROUP BY p.id
+		ORDER BY p.created_at ` + fq.Sort + `
+		LIMIT $2 OFFSET $3;
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryCtxTimeout)
+	defer cancel()
+
+	rows, err := s.db.QueryContext(ctx, query, userId, fq.Limit, fq.Offset, fq.Search, pq.Array(fq.Tags))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var feed []PostWithMetadata
+	for rows.Next() {
+		var p PostWithMetadata
+		err := rows.Scan(
+			&p.ID,
+			&p.UserID,
+			&p.Title,
+			&p.Content,
+			&p.CreatedAt,
+			&p.Version,
+			pq.Array(&p.Tags),
+			&p.User.Username,
+			&p.CommentsCount,
+		)
+		if err != nil {
+			return nil, err
+		}
+		feed = append(feed, p)
+	}
+	return feed, nil
 }
